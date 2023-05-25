ef732cd554064aaaa7a801909246bb7a42363681
Holy-Morphism <ranaadeeltahir@gmail.com>
2023-05-25 12:49:29 +0500

    Routs added

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:mealapp/categories_meal_screen.dart';
+import 'package:mealapp/categories_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Pacifico',
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              fontFamily: 'GustatoryDelightRegular'),
+          bodyMedium: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 20,
+              fontFamily: 'GustatoryDelightRegular'),
+          titleLarge: TextStyle(
+            fontFamily: 'Pacifico',
+          ),
+        ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
+      routes: {'/cat': (context) => CatogeriesMealScreen()},
+      home: const CategoriesScreen(),
     );
   }
 }
